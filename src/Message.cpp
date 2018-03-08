098e48d25ff5beb3dd2a218ecb70d04f007eeb7a
Matthijs <matthijsmud@gmail.com>
2018-03-08 13:26:54 +0100

    Modifed capitalization and const-ness of "Message"  methods.
    Also updated the implementation so that consecutive assignements aren't
    affected by previous ones.

--- src/Message.cpp
+++ src/Message.cpp
@@ -1,28 +1,70 @@
 #include "Message.hpp"
 
-void Message::SetPlayer(int player){
-	internalMessage |= player << 10;
-	RecalcControl();
-}
-void Message::SetData(int weapon){
-	internalMessage |= weapon << 5;
-	RecalcControl();
-}
-void Message::SetTime(int time){
-	internalMessage = time << 6;
-	RecalcControl();
-}
-void Message::StartMessage(){
-	internalMessage = 1<<15;
-	RecalcControl();
+Message::Message():
+// Set the startbit of the message by default.
+internalMessage{1 << 15}
+{
+	
 }
 
-void Message::RecalcControl(){
+Message::Message(uint16_t internalMessage):
+	internalMessage{internalMessage}
+{
+	
+}
+
+uint16_t Message::getMessage() const
+{
+	return internalMessage;
+}
+
+uint16_t Message::getPlayer() const
+{
+	return (internalMessage >> 10) & 31;
+}
+
+void Message::setPlayer(uint16_t player){
+	// Player takes 5 bits starting at position [1] (counting from the most 
+	// significant bit). The maximimum value that can be expressed is 31.
+	
+	// Reset the player bits (X-00000-XXXXX-XXXXX).
+	internalMessage &= ~(31 << 10);
+	internalMessage |= player << 10;
+	calculateChecksum();
+}
+
+uint16_t Message::getData() const
+{
+	// Get masked data (X-?????-XXXXX).
+	return (internalMessage >> 5) & 31;
+}
+
+void Message::setData(uint16_t data){
+	// Data takes 5 bits starting at position [6] (from most significant bit).
+	// 
+	// Reset the data bits (X-XXXXX-00000-XXXXX).
+	internalMessage &= ~(31 << 5);
+	internalMessage |= data << 5;
+	calculateChecksum();
+}
+void Message::setTime(uint16_t time){
+	setData(time);
+}
+void Message::setStartMessage(){
+	setPlayer(0);
+	setTime(0);
+}
+
+bool Message::isStartMessage() const
+{
+	// The gamemaster (id 0) is the only one that can issue the start command.
+	return getPlayer() == 0 && getData() == 0;
+}
+
+void Message::calculateChecksum(){
+	// Reset the checksum bits (X-XXXXX-XXXXX-00000).
+	internalMessage	&= ~(31 << 0);
 	for(int i = 0; i < 5; i++){
 		internalMessage |= (( internalMessage >> (14-i % 16)) & 1) ^ ((internalMessage >> (9-i % 16)) & 1 ) << (4-i);
 	}
-}
-
-uint16_t Message::getMessage(){
-	return internalMessage;
 }
--- src/Message.hpp
+++ src/Message.hpp
@@ -5,16 +5,42 @@
 class Message
 {
 public:
+	Message();
+	// Construct a message from received databits.
 	explicit Message(uint16_t internalMessage);
-	uint16_t getMessage();
-	void SetPlayer(int player);
-	void SetData(int weapon);
-	void SetTime(int time);
-	void StartMessage();
+	// Allow copying of a message, as it is basically a plain old datatype.
+	Message(const Message & other) = default;
+	Message & operator= (const Message & other) = default;
+	
+	
+	//! Gets the underlying data including the checksum. 
+	//!
+	//! The result of this function is intended to be used for a protocol that
+	//! can send it to another device.
+	uint16_t getMessage() const;
+	
+	uint16_t getPlayer() const ;
+	void setPlayer(uint16_t player);
+	
+	//! Gets the data that is sent. The value has little meaning on its own.
+	uint16_t getData() const;
+	//
+	void setData(uint16_t data);
+	
+	//! Gets the time (in minutes) that the match will take.
+	uint16_t getTime() const;
+	//! 
+	//! @param time Time (in minutes) that the game should run.
+	void setTime(uint16_t time);
+	
+	//!
+	void setStartMessage();
+	//! Whether the message is a start command.
+	bool isStartMessage() const;
 
 private:
 	uint16_t internalMessage = 1 << 15;
-	void RecalcControl();
+	void calculateChecksum();
 };
 
 #endif // DEVICES_MESSAGE_HPP
