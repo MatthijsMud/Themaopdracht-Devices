27cfeabf2d6a7a0cbacf7f888d2cfead8a0183eb
robert zegers <r.zegers@xs4all.nl>
2018-03-29 13:39:00 +0200

    added more documentation to sendircontroller and led and deleted unused files

--- src/Led.hpp
+++ src/Led.hpp
@@ -3,9 +3,13 @@
 #include <stdint.h>
 #include "hwlib.hpp"
 
+//! class to set the d2 pin to the right configuration and set it on and off
 class Led{
 	public:
+		//! turn the led on pin d2 on
 		void on();
+
+		//! turn the led on pin d off
 		void off();
 
 	private:
--- src/SendIRController.hpp
+++ src/SendIRController.hpp
@@ -6,6 +6,7 @@
 #include "Message.hpp"
 #include "Led.hpp"
 
+//! task to send an uint16_t message with the specifies ir-protocol
 class SendIRController : public rtos::task<> {
 	private :
 		void main();
@@ -13,9 +14,12 @@ class SendIRController : public rtos::task<> {
 		Led led{};
 
 	public :
-
+		//! channel with the messege this task has to send
 		rtos::channel< uint16_t, 1 > MessageChannel{this, "MessageChannel" };
+
+		//! function for the channel to get the message
 		void RequestSend(uint16_t incommingMessage);
+
 		SendIRController();
 
 };
